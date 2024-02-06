d40ef27519d97a1ceae1299ad796daf9ea755105
Edu1Sinico <EduardoSenai888@gmail.com>
2024-02-06 16:44:24 -0300

    Enviando o primeiro projeto de flutter para o ithub

---
+++ meu_primeiro_flutter/android/app/src/main/kotlin/com/example/meu_primeiro_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.meu_primeiro_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ meu_primeiro_flutter/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TelaOlaMundo(),
+    );
+  }
+}
+
+class TelaOlaMundo extends StatefulWidget {
+  @override
+  _TelaOlaMundoState createState() => _TelaOlaMundoState();
+}
+
+class _TelaOlaMundoState extends State<TelaOlaMundo> {
+  TextEditingController _controllerNome = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Olá Mundo Flutter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Digite seu nome:'),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: TextField(
+                controller: _controllerNome,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                String nome = _controllerNome.text;
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Olá, $nome!')),
+                );
+              },
+              child: Text('Clique para saudar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+

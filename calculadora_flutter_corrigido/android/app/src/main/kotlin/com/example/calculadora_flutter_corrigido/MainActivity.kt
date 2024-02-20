6a7e9d5b0c5477ee3fc5b631b35a7e4eaafc59c2
Edu1Sinico <EduardoSenai888@gmail.com>
2024-02-20 16:08:21 -0300

    Enviando o projeto para o github

---
+++ calculadora_flutter_corrigido/android/app/src/main/kotlin/com/example/calculadora_flutter_corrigido/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.calculadora_flutter_corrigido
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ola_mundo_flutter/android/app/src/main/kotlin/com/example/ola_mundo_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ola_mundo_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

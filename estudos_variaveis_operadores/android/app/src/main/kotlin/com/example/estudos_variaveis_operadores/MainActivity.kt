9e2ee805a128ec72ff812acd7228a0a6cb014175
Edu1Sinico <EduardoSenai888@gmail.com>
2024-02-21 13:21:08 -0300

    Todos os projetos conectados

---
+++ estudos_variaveis_operadores/android/app/src/main/kotlin/com/example/estudos_variaveis_operadores/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.estudos_variaveis_operadores
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ estudos_variaveis_operadores/lib/main.dart
@@ -0,0 +1,13 @@
+void main() {
+  String nome = "João";
+  int idade = 25;
+  double altura = 1.75;
+  bool estudante = true;
+
+  print("Nome: $nome");
+  print("Idade: $idade");
+  print("Altura: $altura metros");
+  print("Estudante: $estudante");
+
+  print("Hello, World!");
+}

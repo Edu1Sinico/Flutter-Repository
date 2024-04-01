a09ef6a471e2e052119b2635ba84bd6aa8f85ba2
Edu1Sinico <EduardoSenai888@gmail.com>
2024-04-01 13:13:30 -0300

    Enviando para o git

---
+++ flutter_android_studio_test/android/app/src/main/kotlin/com/example/flutter_android_studio_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_android_studio_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ projeto_flutter_bd/android/app/src/main/kotlin/com/example/projeto_flutter_bd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.projeto_flutter_bd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()

fa46a53832efc494db486c35c9d414a48b0b07ec
Edu1Sinico <Sinicoes@gmail.com>
2024-04-02 16:57:50 -0300

    Enviando o projeto para o github

---
+++ projeto_sa2_user_login_register/lib/Model/modelUser.dart
@@ -0,0 +1,14 @@
+class modelUser {
+  int id;
+  String name;
+  String email;
+  String senha;
+
+  modelUser({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.senha,
+  });
+  
+}

e82d2fe6bc46e2a6f191c6d644ff3f6ca69c7140
Edu1Sinico <Sinicoes@gmail.com>
2024-05-07 14:07:30 -0300

    Enviando o projeto para o github

--- livraria_json/lib/View/cadastro_livros_view.dart
+++ livraria_json/lib/View/cadastro_livros_view.dart
@@ -19,11 +19,11 @@ class _CadastrarLivroScreenState extends State<CadastrarLivroScreen> {
       TextEditingController();
   final TextEditingController _isbnController = TextEditingController();
   final TextEditingController _valorController = TextEditingController();
-  final TextEditingController _quantidadeController = TextEditingController();
+  final TextEditingController _CapaController = TextEditingController();
+  final TextEditingController _SinopseController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   // Métodos de validações
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -41,49 +41,137 @@ class _CadastrarLivroScreenState extends State<CadastrarLivroScreen> {
                   decoration: InputDecoration(
                     labelText: 'Título do Livro',
                   ),
+                  validator: (value) {
+                    if (value!.trim().isEmpty) {
+                      return 'Título não pode ser vazio!';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 28,
                 ),
                 TextFormField(
                   controller: _autorController,
                   decoration: InputDecoration(
                     labelText: 'Autor',
                   ),
+                  validator: (value) {
+                    if (value!.trim().isEmpty) {
+                      return 'Autor não pode ser vazio!';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 28,
                 ),
                 TextFormField(
                   controller: _editoraController,
                   decoration: InputDecoration(
                     labelText: 'Editora',
                   ),
+                  validator: (value) {
+                    if (value!.trim().isEmpty) {
+                      return 'Editora não pode ser vazia!';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 28,
+                ),
+                TextFormField(
+                  controller: _SinopseController,
+                  decoration: InputDecoration(
+                    labelText: 'Sinopse',
+                  ),
+                  validator: (value) {
+                    if (value!.trim().isEmpty) {
+                      return 'Sinopse não pode ser vazio!';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 28,
                 ),
                 TextFormField(
                   controller: _categoriaController,
                   decoration: InputDecoration(
-                    labelText: 'Categoria',
+                    labelText: 'Categoria, separe por vírgula',
                   ),
+                  validator: (value) {
+                    if (value!.trim().isEmpty) {
+                      return 'Categoria não pode ser vazia!';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 28,
                 ),
                 TextFormField(
                   controller: _anoPublicacaoController,
                   decoration: InputDecoration(
                     labelText: 'Ano de Publicação',
                   ),
+                  validator: (value) {
+                    if (value!.trim().isEmpty) {
+                      return 'Ano de publicação não pode ser vazio!';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 28,
                 ),
                 TextFormField(
                   controller: _isbnController,
                   decoration: InputDecoration(
                     labelText: 'ISBN',
                   ),
+                  validator: (value) {
+                    if (value!.trim().isEmpty) {
+                      return 'ISBN não pode ser vazio!';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 28,
                 ),
                 TextFormField(
                   controller: _valorController,
                   decoration: InputDecoration(
                     labelText: 'Valor',
                   ),
+                  validator: (value) {
+                    if (value!.trim().isEmpty) {
+                      return 'Valor não pode ser vazio!';
+                    } else {
+                      return null;
+                    }
+                  },
                 ),
-                TextFormField(
-                  controller: _quantidadeController,
-                  decoration: InputDecoration(
-                    labelText: 'Quantidade',
-                  ),
+
+                SizedBox(
+                  height: 28,
                 ),
+                ElevatedButton(
+                  child: Text('Cadastrar'),
+                  onPressed: () {
+                    if(_formKey.currentState!.validate()){
+                       _cadastrarLivro();
+                       }},
+                )
               ],
             ),
           ),

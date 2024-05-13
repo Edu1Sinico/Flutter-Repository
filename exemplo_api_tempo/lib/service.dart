40ad60009dd6a83ca00120a35527db8026b8f385
Edu1Sinico <EduardoSenai888@gmail.com>
2024-05-13 16:54:58 -0300

    Enviando o projeto para o github

--- exemplo_api_tempo/lib/service.dart
+++ exemplo_api_tempo/lib/service.dart
@@ -29,4 +29,19 @@ class WeatherService {
       throw Exception('Failed to load weather data');
     }
   }
+
+  // Future<Map<String, dynamic>> getWeatherByLocation(double lat, double lon) async {
+  //   final url = Uri.parse('$baseUrl/weather?lat=${lat}&lon=${lon}&appid=${apikey}');
+
+  //   final response = await http.get(url);
+
+  //   // Verifica se a resposta foi bem-sucedida (código de status 200).
+  //   if (response.statusCode == 200) {
+  //     // Se a resposta foi bem-sucedida, decodifica o corpo da resposta de JSON para um mapa.
+  //     return jsonDecode(response.body);
+  //   } else {
+  //     // Se a resposta não foi bem-sucedida, lança uma exceção indicando o erro.
+  //     throw Exception('Failed to load weather data');
+  //   }
+  // }
 }
